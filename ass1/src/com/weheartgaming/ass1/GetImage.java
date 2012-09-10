af9d5c2f222f3162d9262d735c4531584d1985e6
Fredrik Pettersen <fredrik.fumler@gmail.com>
2012-09-10 04:49:48 +0200

    fixed GetImage, other small fixes
    - GetImage now checks if user has internet, and if they don't it opens
    the settings menu
    - Opens the image in a new thread so it works on other versions than 2.2
    - Minor fixes on ShowLocation, still not able to run getLatitude and
    getLongitude without the program crashing

--- ass1/src/com/weheartgaming/ass1/GetImage.java
+++ ass1/src/com/weheartgaming/ass1/GetImage.java
@@ -1,37 +1,91 @@
 package com.weheartgaming.ass1;
 
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class GetImage extends Activity {
 	ImageView imageview;
+	String imgUrl = "http://hig.no/var/ezwebin_site/storage/images/ansatte/avdeling_for_informatikk_og_medieteknikk/simon_j_r_mccallum/270018-7-nor-NO/simon_j_r_mccallum.jpg";
 	
+	// Runs when the activity is created
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.get_image);
 		
-		imageview = (ImageView) findViewById(R.id.image);
-		Drawable drawable = LoadImagesFromWeb("http://hig.no/var/ezwebin_site/storage/images/ansatte/avdeling_for_informatikk_og_medieteknikk/simon_j_r_mccallum/270018-7-nor-NO/simon_j_r_mccallum.jpg");
-		imageview.setImageDrawable(drawable);
+		// Checks if the user is connected to the internet
+		ConnectivityManager connMgr = (ConnectivityManager)
+				getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		
+		// If connected, show the picture
+		if(networkInfo != null && networkInfo.isConnected()) {
+			imageview = (ImageView) findViewById(R.id.image);
+			new DownloadImage().execute(imgUrl);
+		} 
+		// else notify user that he is not connected, and open wireless and network settings
+		else { 
+			Toast.makeText(getApplicationContext(), "No internet connection availible", Toast.LENGTH_LONG).show();
+			
+
+			startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+			
+			// not sure how to refresh the activity, so finish this activity and go back to main
+			//  when the user user clicks back from the settings page
+			finish();
+
+		}
 
 	}
-	private Drawable LoadImagesFromWeb(String url) {
-		try {
-			InputStream is = (InputStream) new URL(url).getContent();
-			Drawable d = Drawable.createFromStream(is, "src name");
-			return d;
-			
-		} catch(Exception e) {
-			System.out.println("Exc="+e);
-			return null;
+
+	
+	// Creates a new thread and downloads the image in that new thread
+	private class DownloadImage extends AsyncTask <String, Void, Bitmap> {
+		protected Bitmap doInBackground(String...url) {
+			return loadImage(url[0]);
+		}
+		
+		protected void onPostExecute(Bitmap result) {
+			imageview.setImageBitmap(result);
 		}
 	}
 	
+	// Connect to the file trough http and put in an inputstream, then convert it to bitmap
+	public Bitmap loadImage(final String s) {
+		Bitmap bm = null;
+		try {
+			URL url = new URL(s);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.connect();
+			InputStream is = conn.getInputStream();
+			bm = BitmapFactory.decodeStream(is);
+		} catch (Exception e) {
+			// Get logged message if it fails
+			System.out.println("Failed to get image: " + e);
+			e.printStackTrace();
+		}
+		return bm;
+	}
+
+	
 }
+
+
                                                           
--- ass1/src/com/weheartgaming/ass1/ShowLocation.java
+++ ass1/src/com/weheartgaming/ass1/ShowLocation.java
@@ -1,20 +1,70 @@
 package com.weheartgaming.ass1;
 
 import android.app.Activity;
-import android.content.Context;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.Toast;
 
 public class ShowLocation extends Activity {
-
+	private ProgressDialog progressDialog;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_location);
         
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        
+        if(lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        	runDialog(5);
+        	Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        	
+        	
+        	if(location != null)
+        	{
+        		location.getLatitude();
+        		location.getLongitude();
+        	}
+        	
+        	String coord = "geo:"+ location.getLatitude() + "," + location.getLongitude();
+        	
+        	
+        	//String uri = "geo:"+ lat + "," + lon;
+        	
+
+        	startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(coord)));
+
+        	
+        	
+        	//Toast.makeText(getApplicationContext(), "GPS is turned on!", Toast.LENGTH_LONG).show();
+        } else {
+        	Toast.makeText(getApplicationContext(), "GPS is turned off!", Toast.LENGTH_LONG).show();
+        	
+        	startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+        	
+        	finish();
+        }
+    }
+    
+    private void runDialog(final int seconds)
+    {
+        	progressDialog = ProgressDialog.show(this, "Please wait....", "Updating location");
+
+        	new Thread(new Runnable(){
+        		public void run(){
+        			try {
+        				Thread.sleep(seconds * 1000);
+    					progressDialog.dismiss();
+    				} catch (InterruptedException e) {
+    					e.printStackTrace();
+    				}
+        		}
+        	}).start();
     }
 
 }
